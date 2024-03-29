22ff020c39e0fbecf21df91802dc5418924356eb
Ash <128718645+S-Axhwin@users.noreply.github.com>
2024-03-30 04:02:39 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { ThemeProvider } from "@/components/theme-provider"
 import { Provider } from 'react-redux'
 import store from './redux/store.tsx'
@@ -9,9 +9,9 @@ import store from './redux/store.tsx'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-    <BrowserRouter basename='OffClient'>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </ThemeProvider>
   </Provider>
   
