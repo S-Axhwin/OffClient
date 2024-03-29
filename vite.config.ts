f3f3eb514c0761cbb9b2156519e821222eb9daac
Ash <128718645+S-Axhwin@users.noreply.github.com>
2024-03-30 03:33:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/OffIciumClient/",
+  base: "/OffIciumClient",
+  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
